7146cd4b65815a85ae947402bff4d304f9058bf0
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-05 15:32:31 +0530

    Linked into App Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LikeButtonComponentComponent } from './like-button-component/like-button-component.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LikeButtonComponentComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule
